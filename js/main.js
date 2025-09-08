c748079b1827e8403fd7686958a1a85b65b84d19
Dastan Hairushev <hairushevdastan@gmail.com>
2025-09-08 19:29:18 +0500

    feat: add base structure cv

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
-if(1 - (window.scrollY / 700) < 0) {
-  document.getElementById('particles-js').style.transition = 'all 0s'
-  document.getElementById('particles-js').style.opacity = 0;
+// ================================== Background Opacity ===============================
+
+let particlesBg = document.getElementById('particles-js');
+
+if(1 - (window.scrollY / 500) < 0) {
+  particlesBg.style.display = 'none';
+  particlesBg.style.opacity = 0;
 }
 
 window.addEventListener("scroll", () => {
-  let opacity = 1 - (window.scrollY / 700);
-  if(window.scrollY < 0 || window.scrollY < 1) {
+  let opacity = 1 - (window.scrollY / 500);
+  if(opacity < -0.5 || opacity > 1) {
+    particlesBg.style.opacity = 0;
+    particlesBg.style.display = 'none';
     return
   } 
-  document.getElementById('particles-js').style.opacity = opacity;
-  document.getElementById('particles-js').style.transition = 'all 0.3s'
+  particlesBg.style.display = 'block';
+  particlesBg.style.opacity = opacity;
+  particlesBg.style.transition = 'all 0.3s'
+})
+
+// ================================== Ahmad Logo ===============================
+
+let ahmadSound = document.getElementById('ahmad__sound');
+let logo = document.querySelector('.logo')
+
+ahmadSound.volume = 0.2;
+
+logo.addEventListener('mouseover', () => {
+  ahmadSound.play()
+})
+logo.addEventListener('mouseout', () => {
+  ahmadSound.pause()
 })
